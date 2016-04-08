0d9153e0a8b0d014e8b4307055f5a2b8a05ced6b
Yuhang Wang <stevenaura@outlook.com>
2016-04-08 11:21:09 -0500

    correction: previous sample pdb was DLPC instead of POPC; I have just replaced it with a new POPC from CHARMM gui

--- test/MolecularBuild/unit_test_4_build_lipid_grid.jl
+++ test/MolecularBuild/unit_test_4_build_lipid_grid.jl
@@ -1,10 +1,10 @@
-test_build_lipid_grid(
+@time test_build_lipid_grid(
     joinpath(test_dir(:data),"lipid","popc.pdb"),
-    joinpath(test_dir(:tmp),"new_3d.pdb"),
+    joinpath(test_dir(:tmp),"20x20x8_popc.pdb"),
     MolecularPDB.VMD,
     Grid,
     Array[[1., 0., 0],[0., 1., 0], [0., 0., 1.]],
-    (5., 5., 20.),
-    (20, 20, 10),
+    (10., 10., 25.),
+    (20, 20, 8),
     "3D grid"
     )
